d7b2b4591e9614eb2b81857bda8859a8b5207a6c
ItzPatoo <lo.gu2005@hotmail.it>
2019-02-25 19:33:27 +0100

    modified path for cookies

--- js/main.js
+++ js/main.js
@@ -113,7 +113,7 @@ function setCookie(name, value, exdays) {
   var d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
   var expires = "expires=" + d.toGMTString();
-  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+  document.cookie = name + "=" + value + ";" + expires + ";path=/tg-js-bot";
 }
 
 function getCookies() {
