88fb2b10fa4ae11dbf2dc4ab8fd52a8ac6f0e929
ItzPatoo <lo.gu2005@hotmail.it>
2019-02-26 16:52:22 +0100

    Added console commands to send and receive messages (use /help) Added /chatid command Added autoStart at page load option

--- js/main.js
+++ js/main.js
@@ -7,26 +7,59 @@ var updateAnalyzer;
 var cookies = localStorage;
 var commands = {};
 var started = 0;
+var selectedChatId = 0;
+var lastCommand = "";
+function log(text, prefix = "[INFO]") {
+  var consoleElem = $("#console");
+  consoleElem.val(consoleElem.val() + prefix + " " + text + "\n");
+  consoleElem.scrollTop(consoleElem[0].scrollHeight - consoleElem.height());
+}
 $(document).ready(function() {
-  if("commands" in cookies) {
-    $("#commands").val(cookies["commands"].split("///////////").join("\n"));
-  } else $("#commands").val("/start > Messaggio di avvio!;\n/help > Menù di aiuto!;");
-  if("botToken" in cookies) {
-    $("#token").val(cookies["botToken"]);
-  }
-  if("botSettings" in cookies) {
-    var bSettings = JSON.parse(cookies["botSettings"]);
-    if ("parseMode" in bSettings)
-      $("#parseMode").val(bSettings["parseMode"]);
-    if ("wpPreview" in bSettings)
-      $("#wpPreview").val(bSettings["wpPreview"]);
-  }
-  M.textareaAutoResize($('#commands'));
-  M.updateTextFields();
-  $("#console").val("");
-  $(".tooltipped").tooltip();
-  $('select').formSelect();
-  updateCommands(false);
+  $("#autoStart").on("change", updateBotSettings);
+  $("#parseMode").on("change", updateBotSettings);
+  $("#wpPreview").on("change", updateBotSettings);
+  $("#consoleCommandsGo").click(function() {
+    var command = $("#consoleCommands");
+    lastCommand = command.val();
+    switch(command.val().split(" ",1)[0]) {
+      case "/help":
+        log("Menù comandi:\n/help: visualizza questo menù di aiuto\n/select <chat_id>: seleziona chat in cui inviare i messaggi\n<messaggio>: invia messaggio nella chat_id selezionata", "");
+        break;
+      case "/select":
+        var cId = command.val().split(" ");
+        if (1 in cId) {
+          selectedChatId = cId[1];
+          log("Selezionato "+selectedChatId+"!", "[!]");
+          updateBotSettings();
+        } else {
+          if(selectedChatId) {
+            selectedChatId = 0;
+            log("Rimossa selezione chat_id.")
+          } else
+            log("Metti la chat_id dopo /select, se non conosci il chat_id, dai al bot /chatid", "[ERRORE]");
+        }
+        break;
+      default:
+        if(command.val().charAt(0) == "/")
+          log("Comando non valido.", "[ERRORE]");
+        else {
+          if (selectedChatId != 0) {
+            log("Invio del messaggio in "+selectedChatId+"...");
+            sendMessage(selectedChatId, command.val(), true);
+          } else
+            log("Per favore, prima di tentare di inviare un messaggio, usa /select", "[ERRORE]");
+        }
+        break;
+    }
+    command.val("");
+  });
+  $("#consoleCommands").keyup(function(e) {
+    if(e.keyCode == 13) {
+      $("#consoleCommandsGo").click();
+    } else if(e.keyCode == 38) {
+      $(this).val(lastCommand);
+    }
+  });
   $("#startBot").click(function() {
     botToken = $("#token").val();
     if(botToken != "" && botToken) {
@@ -42,15 +75,54 @@ $(document).ready(function() {
     $("#stopBot").addClass("disabled");
     log("Richiesta di arresto inviata!");
   });
+  $("#commands").blur(function() {
+    updateCommands(true);
+  });
   $("#updateCommands").click(updateCommands);
+  if("commands" in cookies) {
+    $("#commands").val(cookies["commands"].split("///////////").join("\n"));
+  } else $("#commands").val("/start > Messaggio di avvio!;\n/help > Menù di aiuto!;");
+  if("botToken" in cookies) {
+    $("#token").val(cookies["botToken"]);
+    botToken = $("#token").val();
+  }
+  if("botSettings" in cookies) {
+    var bSettings = JSON.parse(cookies["botSettings"]);
+    if ("parseMode" in bSettings)
+      $("#parseMode").val(bSettings["parseMode"]);
+    if ("wpPreview" in bSettings)
+      $("#wpPreview").val(bSettings["wpPreview"]);
+    if ("autoStart" in bSettings) {
+      $("#autoStart").prop("checked", bSettings["autoStart"]);
+      if(bSettings["autoStart"] == true) {
+        setTimeout(function() {
+          log("Bot in avvio secondo le tue impostazioni...");
+          if (botToken != "" && botToken) {
+            $("#startBot").click();
+          } else {
+            log("Bot token vuoto.");
+            $("#autoStart").prop("checked", false);
+            updateBotSettings();
+          }
+        }, 3000);
+      }
+    }
+    if ("selectedChatId" in bSettings) {
+      selectedChatId = bSettings["selectedChatId"];
+      setTimeout(function() {
+        log("Selezionata chat_id "+selectedChatId+" come da sessione precedente.");
+      }, 1000)
+    }
+  }
+  M.textareaAutoResize($('#commands'));
+  M.updateTextFields();
+  $("#console").val("");
+  $(".tooltipped").tooltip();
+  $('select').formSelect();
+  updateCommands(false);
 });
-function log(text) {
-  var consoleElem = $("#console");
-  consoleElem.val(consoleElem.val() + text + "\n");
-  consoleElem.scrollTop(consoleElem.height());
-}
 function updateCommands(doLog = true) {
-  $(this).addClass("disabled");
+  $("#updateCommands").addClass("disabled");
   if(doLog) log("Aggiornamento lista comandi in corso...");
   commands = {};
   var commandsString = $("#commands").val();
@@ -62,7 +134,7 @@ function updateCommands(doLog = true) {
   }
   localStorage.setItem("commands", $("#commands").val().replace(/\n/g, "///////////"));
   if(doLog) log("Aggiornamento lista comandi completato!");
-  $(this).removeClass("disabled");
+  $("#updateCommands").removeClass("disabled");
 }
 function startUpdateAnalyzer() {
   $.ajax({
@@ -91,10 +163,6 @@ function startUpdateAnalyzer() {
       }
       if(started == 0) {
         localStorage.setItem("botToken", $("#token").val());
-        localStorage.setItem("botSettings", JSON.stringify({
-          parseMode: $("#parseMode").val(),
-          wpPreview: $("#wpPreview").val()
-        }));
         log("Bot avviato! Attenzione: Se chiudi questa pagina, verrà anche arrestato il tuo bot!");
         $("#stopBot").removeClass("disabled");
         started = 1;
@@ -103,29 +171,56 @@ function startUpdateAnalyzer() {
         $("#startBot").removeClass("disabled");
         log("Bot arrestato!");
         started = 0;
-      } else startUpdateAnalyzer();
+      } else setTimeout(startUpdateAnalyzer, 500);
     }
   });
+}
+function updateBotSettings() {
+  if (botToken != "" && botToken)
+    localStorage.setItem("botSettings", JSON.stringify({
+      parseMode: $("#parseMode").val(),
+      wpPreview: $("#wpPreview").val(),
+      autoStart: $("#autoStart").prop("checked"),
+      selectedChatId: selectedChatId
+    }));
 }
 function analyzeUpdate(update) {
   var text = update["message"]["text"];
   var chat_id = update["message"]["chat"]["id"];
+  if(selectedChatId == chat_id) {
+    log(text, "[M: "+update.message.from.first_name+(update.message.from.last_name ? " "+update.message.from.last_name : "")+"]")
+  }
+  if(text == "/chatid") {
+    sendMessage(chat_id, "ID del gruppo: <code>"+chat_id+"</code>", false, "HTML");
+  }
   if(text in commands) {
+    sendMessage(chat_id, commands[text]);
+  }
+}
+function sendMessage(chat_id, messageText, doLog = false, parse_mode = false) {
+  if(!parse_mode) parse_mode = $("#parseMode").val();
+  if((chat_id == undefined || chat_id == "") && !chat_id) {
+    return false;
+  } else {
     $.ajax({
       url: "https://api.telegram.org/bot" + botToken + "/sendMessage",
       async: true,
       method: "POST",
       data: {
         chat_id: chat_id,
-        text: commands[text],
-        parse_mode: $("#parseMode").val(),
+        text: messageText,
+        parse_mode: parse_mode,
         disable_web_page_preview: $("wpPreview").val()
       },
       dataType: "json",
+      success: function(response) {
+        if(doLog) log("Messaggio inviato in "+chat_id, "[!]");
+      },
       error: function(xhr) {
         var response = xhr.responseText;
-        log("Errore nell'invio del messaggio: "+response);
+        log("Errore nell'invio del messaggio: "+response, "[ERRORE]");
       },
     });
+    return true;
   }
 }
